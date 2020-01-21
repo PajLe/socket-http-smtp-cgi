65d0935c6673482e0a08db10bf3d399cbbe39138
PajLe <pavle.pajle@gmail.com>
2020-01-21 22:07:46 +0100

    Create cgi test server.cpp

---
+++ cgi test server.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main() 
+{
+
+  // TEST
+	/*//printf("Content-Length: 170\r\n"); 
+	printf("Content-Type: text/html\n\n"); 
+	fflush(stdout);
+	printf("<!DOCTYPE html>\n");
+	printf("<html>\n");
+	printf("<head>\n");
+	printf("<title>\n");
+	printf("asdf\n");
+	printf("</title>\n");
+	printf("<h1>ffewfwefew</h1>\n");
+	printf("</head>\n");
+	printf("</html>\n");
+	getenv("QUERY_STRING");
+	fflush(stdout);*/
+
+	
+  // cgi-bin/Calculator.exe 
+	char znak[1024];
+  // parsing not implemented
+	scanf("%s", znak);
+	printf("6969");
+	return 0;
+}
